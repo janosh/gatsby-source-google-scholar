82cabd6fc25a543738cb5f3666836e79a32690b6
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-12-24 16:22:16 +0000

    refactor: scraper error handling

--- scraper.js
+++ scraper.js
@@ -8,34 +8,39 @@ const cheerio = require('cheerio')
 const processResults = require('./processor')
 
 const resultsPerPage = 10
-
 const googleScholarUrl = 'https://scholar.google.com/scholar?hl=en&q='
 const scholarUrl = 'https://scholar.google.com'
 
 const statusCodeForRateLimit = 503
 const statusMessageForRateLimit = 'Service Unavailable'
-const statusMessageBody =
-  'This page appears when Google automatically detects requests coming from your computer network which appear to be in violation of the <a href="//www.google.com/policies/terms/">Terms of Service</a>. The block will expire shortly after those requests stop.'
+const statusMessageBody = `
+  This page appears when Google automatically detects requests
+  coming from your computer network which appear to be in violation
+  of the <a href="//www.google.com/policies/terms/">Terms of Service</a>.
+  The block will expire shortly after those requests stop.
+`
 
 // regex with thanks to http://stackoverflow.com/a/5917250/1449799
 const resultCountRegex = /\W*((\d+|\d{1,3}(,\d{3})*)(\.\d+)?) results/
 
 const scholarResultsCallback = (resolve, reject) => (error, response, body) => {
-  if (error) {
-    reject(error)
-  } else if (response.statusCode !== 200) {
+  if (error) return reject(error)
+  if (response.statusCode !== 200) {
     if (
       response.statusCode === statusCodeForRateLimit &&
       response.statusMessage === statusMessageForRateLimit &&
       response.body.includes(statusMessageBody)
     ) {
-      reject(
-        new Error(
-          'You have made too many requests too quickly and are being rate-limited by Google Scholar. See: https://support.google.com/websearch/answer/86640.'
-        )
+      return reject(
+        new Error(`
+          You have made too many requests too quickly and are being rate-limited
+          by Google Scholar. See: https://support.google.com/websearch/answer/86640.
+          You might try to visit scholar.google.com and solve a captcha to unblock your IP
+          or use a VPN.
+        `)
       )
     } else {
-      reject(
+      return reject(
         new Error(
           `Expected status code 200 on Google Scholar http response, but got ${
             response.statusCode
@@ -43,50 +48,55 @@ const scholarResultsCallback = (resolve, reject) => (error, response, body) => {
         )
       )
     }
-  } else {
-    const html = cheerio.load(body)
-    const results = html('.gs_r.gs_or.gs_scl')
-    if (!results.length)
-      return reject(new Error('your Google Scholar query returned no results'))
-
-    const nextUrl =
-      scholarUrl +
-      html('.gs_ico_nav_next')
-        .parent()
-        .attr('href')
-    const prevUrl =
-      scholarUrl +
-      html('.gs_ico_nav_previous')
-        .parent()
-        .attr('href')
-
-    const resultsCountString = html('#gs_ab_md').text()
-    const matches = resultCountRegex.exec(resultsCountString)
-    const resultCount = parseInt(matches[1].replace(/,/g, ''))
-
-    resolve({
-      results: processResults(html, results),
-      count: resultCount,
-      nextUrl,
-      prevUrl,
-      next: () =>
-        new Promise((resolve, reject) => {
-          const requestOptions = {
-            jar: true,
-            url: nextUrl,
-          }
-          request(requestOptions, scholarResultsCallback(resolve, reject))
-        }),
-      previous: () =>
-        new Promise((resolve, reject) => {
-          const requestOptions = {
-            jar: true,
-            url: prevUrl,
-          }
-          request(requestOptions, scholarResultsCallback(resolve, reject))
-        }),
-    })
   }
+  const html = cheerio.load(body, { decodeEntities: true })
+  const results = html('.gs_r.gs_or.gs_scl')
+  if (!results.length)
+    return reject(
+      new Error(`
+        Your Google Scholar query returned no results. Google may
+        be rate-limiting your requests. You may try to visit 
+        scholar.google.com and solve a captcha to unblock your IP or use a VPN.
+      `)
+    )
+
+  const nextUrl =
+    scholarUrl +
+    html('.gs_ico_nav_next')
+      .parent()
+      .attr('href')
+  const prevUrl =
+    scholarUrl +
+    html('.gs_ico_nav_previous')
+      .parent()
+      .attr('href')
+
+  const resultsCountString = html('#gs_ab_md').text()
+  const matches = resultCountRegex.exec(resultsCountString)
+  const resultCount = parseInt(matches[1].replace(/,/g, ''))
+
+  resolve({
+    results: processResults(html, results),
+    count: resultCount,
+    nextUrl,
+    prevUrl,
+    next: () =>
+      new Promise((resolve, reject) => {
+        const requestOptions = {
+          jar: true,
+          url: nextUrl,
+        }
+        request(requestOptions, scholarResultsCallback(resolve, reject))
+      }),
+    previous: () =>
+      new Promise((resolve, reject) => {
+        const requestOptions = {
+          jar: true,
+          url: prevUrl,
+        }
+        request(requestOptions, scholarResultsCallback(resolve, reject))
+      }),
+  })
 }
 
 const search = query =>
