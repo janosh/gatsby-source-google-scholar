c84a77918627f956402617186edba88786f0de17
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-12-28 15:28:28 +0000

    style: create theme

---
+++ example/src/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  darkRed: `#c50000`,
+  lightGreen: `#00c5b7`,
+  orange: `#d3b300`,
+  lightGray: `lightgray`,
+  veryLightGray: `whitesmoke`,
+  darkPurple: `#00004d`,
+  darkBlue: `#004797`,
+  white: `white`,
+}
