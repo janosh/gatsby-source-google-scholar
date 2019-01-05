96a6c03d69c9c3b691fffa60424e3071b433a091
merelinguist <merelinguist@users.noreply.github.com>
2019-01-05 11:29:34 +0000

    Add meta tags

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -2,16 +2,17 @@ module.exports = {
   siteMetadata: {
     title: `gatsby-source-google-scholar-example`,
     description: `gatsby-source-google-scholar kitchen sink`,
-    author: `Janosh Riebesell`,
+    author: `Janosh Riebesell`
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     {
       // locally importing gatsby-source-google-scholar
       resolve: require.resolve(`..`),
       options: {
-        queries: [`richard feynman`, `albert einstein`],
-      },
-    },
-  ],
+        queries: [`richard feynman`, `albert einstein`]
+      }
+    }
+  ]
 }
